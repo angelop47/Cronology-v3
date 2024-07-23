3278f226e11cadc901e70afa7f513291a0593f82
Angelo Pescetto <angelopescetto@192.168.1.6>
2024-07-23 19:31:15 -0300

    login and logout

--- cronologia-v3/src/components/useAuth.js
+++ cronologia-v3/src/components/useAuth.js
@@ -1,28 +1,83 @@
-// src/useAuth.js
-import React, { useState, useEffect, createContext, useContext } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../utils/firebase-config";
+// src/components/useAuth.js
+import React, { createContext, useContext, useState, useEffect } from "react";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+} from "firebase/auth";
+import axios from "axios";
+import { auth, provider } from "../utils/firebase-config";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setUser(user);
-      setLoading(false);
-      console.log("User state changed:", user);
+    const auth = getAuth();
+    const unsubscribe = auth.onAuthStateChanged(async (firebaseUser) => {
+      if (firebaseUser) {
+        const userData = {
+          name: firebaseUser.displayName,
+          email: firebaseUser.email,
+          photo: firebaseUser.photoURL,
+        };
+
+        // Guardar o actualizar el usuario en MongoDB
+        await axios.post(
+          "https://cronology-v3.onrender.com/users/auth/google",
+          userData
+        );
+
+        setUser(firebaseUser);
+      } else {
+        setUser(null);
+      }
     });
 
-    return () => {
-      unsubscribe();
-    };
+    return () => unsubscribe();
   }, []);
 
+  const login = async () => {
+    const auth = getAuth();
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Datos del usuario
+      const userData = {
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL,
+      };
+
+      // Llamar a la API de tu backend para guardar o actualizar el usuario en MongoDB
+      await axios.post(
+        "https://cronology-v3.onrender.com/users/auth/google",
+        userData
+      );
+
+      setUser(user);
+      console.log("Usuario autenticado y datos guardados");
+    } catch (error) {
+      console.error("Error al iniciar sesión:", error);
+    }
+  };
+
+  const logout = async () => {
+    const auth = getAuth();
+    try {
+      await signOut(auth);
+      setUser(null);
+      console.log("Usuario cerrado sesión");
+    } catch (error) {
+      console.error("Error al cerrar sesión:", error);
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ user, loading }}>
+    <AuthContext.Provider value={{ user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
--- cronologia-v3/src/screens/Login.jsx
+++ cronologia-v3/src/screens/Login.jsx
@@ -1,71 +1,32 @@
+// src/components/Login.js
 import React from "react";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import axios from "axios";
+import { useAuth } from "../components/useAuth";
 
 const Login = () => {
-  const provider = new GoogleAuthProvider();
-
-  const handleLoginClick = async () => {
-    const auth = getAuth();
-
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-
-      // Datos del usuario
-      const userData = {
-        name: user.displayName,
-        email: user.email,
-        photo: user.photoURL,
-      };
-
-      // Llamar a la API de tu backend para guardar o actualizar el usuario en MongoDB
-      await axios.post(
-        "https://cronology-v3.onrender.com/users/auth/google",
-        userData
-      );
-
-      console.log("Usuario autenticado y datos guardados");
-    } catch (error) {
-      console.error("Error al iniciar sesión:", error);
-    }
-  };
+  const { user, login, logout } = useAuth();
 
   return (
-    <div>
-      <button
-        onClick={handleLoginClick}
-        className="btn bg-blue-600 text-white font-semibold px-3 py-1 rounded duration-500"
-      >
-        Iniciar sesión con Google
-      </button>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      {!user ? (
+        <button
+          onClick={login}
+          className="btn bg-blue-600 text-white font-semibold px-3 py-1 rounded duration-500"
+        >
+          Iniciar sesión con Google
+        </button>
+      ) : (
+        <div className="text-center">
+          <p className="mb-4">Bienvenido, {user.displayName}</p>
+          <button
+            onClick={logout}
+            className="btn bg-red-600 text-white font-semibold px-3 py-1 rounded duration-500"
+          >
+            Cerrar sesión
+          </button>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Login;
-
-// // src/components/Login.js
-// import React from "react";
-// import { useAuth } from "../components/useAuth";
-// import LoginButton from "../components/LoginButton";
-// import LogoutButton from "../components/LogoutButton";
-
-// const Login = () => {
-//   const { user } = useAuth();
-
-//   return (
-//     <div>
-//       {!user ? (
-//         <LoginButton />
-//       ) : (
-//         <div>
-//           <p>Bienvenido, {user.displayName}</p>
-//           <LogoutButton />
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Login;
